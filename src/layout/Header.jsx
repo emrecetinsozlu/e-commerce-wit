2fd529f0f45e43bd3f3cbee98b6d27f88807d412
emrecetinsozlu <emrecetinsozlu@gmail.com>
2025-12-26 16:43:04 +0300

    hero section a başlandı

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -11,8 +11,31 @@ function Header() {
   return (
     <>
     <header className='px-8 py-4'>
-      <div className='flex justify-between py-2'>
-        <h3 className='text-2xl font-bold text-[#252B42]'>Bandage</h3>
+     
+      <div className='flex justify-between items-center'>
+        <h3 className='text-2xl font-bold text-[#252B42] m-0'>Bandage</h3>
+        <nav className='hidden md:block'>
+          <ul className='flex gap-6 font-normal'>
+              <li className='my-7'>
+                  <NavLink exact to='/' className='text-[#737373]'>Home</NavLink>
+              </li>
+            <li className='my-7'>
+                <NavLink to='/shop' className='text-[#737373]'>Shop</NavLink>
+            </li>
+            <li className='my-7'>
+                <NavLink to='/about' className='text-[#737373]'>About</NavLink>
+            </li>
+            <li className='my-7'>
+                <NavLink to='/blog' className='text-[#737373]'>Blog</NavLink>
+            </li>
+            <li className='my-7'>
+                <NavLink to='/contact' className='text-[#737373]'>Contact</NavLink>
+            </li>
+             <li className='my-7'>
+                <NavLink to='/pages' className='text-[#737373]'>Pages</NavLink>
+            </li>
+          </ul>
+        </nav>
         <div className='flex gap-6 '>
           <button>
             <UserRound size={24} />
@@ -29,6 +52,7 @@ function Header() {
         </div>
       </div>
     </header>
+
     {isOpen && (
       <nav className='font-normal text-3xl w-full text-center'>
         <ul className=''>
--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -8,11 +8,11 @@ function Home() {
         <Header />
         <section>
             <div className='w-full relative'>
-                <img src="../../public/product-slide-1.jpg" alt=""/>
-                <div className='absolute top-0 left-0 flex flex-col justify-center items-center w-full h-full gap-6 text-white'>
-                    <h1>GROCERIES <br /> DELIVERY</h1>
+                <img src=" /hero_image.jpg" alt="" className='w-full object-cover md:h-lvh '/>
+                <div className='absolute top-0 left-0 flex flex-col justify-center items-center w-full h-full gap-6 text-white text-center bg-black/40'>
+                    <h1 className="text-xl font-bold max-w-[200px] md:max-w-none break-words leading-tight"> GROCERIES DELIVERY</h1>
                     <p>We know how large objects <br />will act, but things on a small scale</p>
-                    <button> Start Now</button>
+                    <button className='button-sm md:button-mb'> Start Now</button>
                 </div>
               
             </div>
